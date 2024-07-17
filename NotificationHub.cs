fad590013cdd3992d5ba73578a2a0e82d0b597c1
Roberto Borges <roberto.borges@outlook.com>
2024-07-17 14:21:29 -0400

    first commit

---
+++ NotificationHub.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRApp.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
